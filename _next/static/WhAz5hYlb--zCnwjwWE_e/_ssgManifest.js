d92b2efbdb1618173385e3fbcd869c66c21e1349
sultanardiansyah77 <168266625+sultanardiansyah77@users.noreply.github.com>
2024-04-29 14:22:19 +0700

    Add files via upload

---
+++ _next/static/WhAz5hYlb--zCnwjwWE_e/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002Fdigital\u002F[id]","\u002Fposts","\u002Fposts\u002F[slug]","\u002Fsearch\u002F[productCategory]","\u002Ftags\u002F[tagName]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
